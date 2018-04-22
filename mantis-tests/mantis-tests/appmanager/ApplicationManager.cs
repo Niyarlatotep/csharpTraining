d52c407b24344cb1d3e1f7e0828a1285c6ee12d9
Niyarlatotep <niyarlatotep@gmail.com>
2018-04-22 14:45:51 +0300

    Mantis account creation

---
+++ mantis-tests/mantis-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace mantis_tests
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+       
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager() {
+            FirefoxOptions options = new FirefoxOptions();
+            options.UseLegacyImplementation = true;
+            options.BrowserExecutableLocation = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+            driver = new FirefoxDriver(options);
+            baseURL = "http://localhost";
+            Registration = new RegistrationHelper(this);
+            Ftp = new FtpHelper(this);
+        }
+
+        ~ApplicationManager()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (!app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.driver.Url = "http://localhost/mantisbt-1.2.17/login_page.php";
+                app.Value = newInstance;
+            }
+            return app.Value;
+        }
+       
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
+            }
+        }
+
+        public string BaseURL
+        {
+            get
+            {
+                return baseURL;
+            }
+        }
+
+        public RegistrationHelper Registration { get; private set; }
+        public FtpHelper Ftp { get; private set; }
+    }
+}
